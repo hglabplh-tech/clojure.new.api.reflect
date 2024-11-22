93d7abba1fd5144b0b98d42fdb1e0c226c40d41f
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2024-11-22 09:27:37 +0000

    fixed minor bad behaviour

--- src/test/clj/io/github/hglabplh_tech/reflect/clojure/api/relect_special_forms_tests.clj
+++ src/test/clj/io/github/hglabplh_tech/reflect/clojure/api/relect_special_forms_tests.clj
@@ -3,39 +3,50 @@
             [clojure.test :refer :all]
             [io.github.hglabplh-tech.reflect.clojure.api.reflect-class :as rcl]
             [io.github.hglabplh-tech.reflect.clojure.api.reflect-special-forms :as sform]))
-(def enum-check-vect [:enum-spec
-                      {"TOOLS"
-                       {:ordinal-val    0,
-                        :class-descr-val
-                        "Lio/github/hglabplh_tech/reflect/clojure/api/example/annot_rec/Category;",
-                        :enem-descr-val "EnumDesc[Category.TOOLS]"},
-                       "APIS"
-                       {:ordinal-val    1,
-                        :class-descr-val
-                        "Lio/github/hglabplh_tech/reflect/clojure/api/example/annot_rec/Category;",
-                        :enem-descr-val "EnumDesc[Category.APIS]"},
-                       "TESTS"
-                       {:ordinal-val    2,
-                        :class-descr-val
-                        "Lio/github/hglabplh_tech/reflect/clojure/api/example/annot_rec/Category;",
-                        :enem-descr-val "EnumDesc[Category.TESTS]"},
-                       "PROJECT"
-                       {:ordinal-val    3,
-                        :class-descr-val
-                        "Lio/github/hglabplh_tech/reflect/clojure/api/example/annot_rec/Category;",
-                        :enem-descr-val "EnumDesc[Category.PROJECT]"},
-                       "NONE"
-                       {:ordinal-val    4,
-                        :class-descr-val
-                        "Lio/github/hglabplh_tech/reflect/clojure/api/example/annot_rec/Category;",
-                        :enem-descr-val "EnumDesc[Category.NONE]"}}])
+(def enum-check-map {:enum-spec
+                      {"HOST"
+                       {:ordinal 0,
+                        :class-descr
+                        "Lio/github/hglabplh_tech/reflect/clojure/api/app_exam/AppConfigFields;",
+                        :enum-descr "EnumDesc[AppConfigFields.HOST]"},
+                       "PORT"
+                       {:ordinal 1,
+                        :class-descr
+                        "Lio/github/hglabplh_tech/reflect/clojure/api/app_exam/AppConfigFields;",
+                        :enum-descr "EnumDesc[AppConfigFields.PORT]"},
+                       "SSL_PORT"
+                       {:ordinal 2,
+                        :class-descr
+                        "Lio/github/hglabplh_tech/reflect/clojure/api/app_exam/AppConfigFields;",
+                        :enum-descr "EnumDesc[AppConfigFields.SSL_PORT]"},
+                       "URL_SPEC"
+                       {:ordinal 3,
+                        :class-descr
+                        "Lio/github/hglabplh_tech/reflect/clojure/api/app_exam/AppConfigFields;",
+                        :enum-descr "EnumDesc[AppConfigFields.URL_SPEC]"},
+                       "USER"
+                       {:ordinal 4,
+                        :class-descr
+                        "Lio/github/hglabplh_tech/reflect/clojure/api/app_exam/AppConfigFields;",
+                        :enum-descr "EnumDesc[AppConfigFields.USER]"},
+                       "IP"
+                       {:ordinal 5,
+                        :class-descr
+                        "Lio/github/hglabplh_tech/reflect/clojure/api/app_exam/AppConfigFields;",
+                        :enum-descr "EnumDesc[AppConfigFields.IP]"}}})
+
+(def record-check-map {:test [java.lang.Integer],
+                       :first-name [java.lang.String],
+                       :last-name [java.lang.String],
+                       :ctor-parm-type [java.lang.Integer java.lang.String java.lang.String]})
+
 (deftest enum.analyze.test
   (testing "Here the enum reflection transform is tested"
     (let [class-util (rcl/get-class-util
                        "io.github.hglabplh_tech.reflect.clojure.api.app_exam.AppConfigFields")
           result (sform/analyze-enum (rcl/get-the-class class-util))]
       (pp/pprint result)
-      ;; (is (= enum-check-vect result))
+      (is (= enum-check-map result))
       )))
 
 (deftest record.analyze.test
@@ -44,7 +55,7 @@
                        "io.github.hglabplh_tech.reflect.clojure.api.example.annot_rec.MyTestRecord")
           result (sform/analyze-record (rcl/get-the-class class-util))]
       (pp/pprint result)
-      ;; (is (= enum-check-vect result))
+      (is (= record-check-map result))
       )
     ))
 
--- src/test/java/io/github/hglabplh_tech/reflect/clojure/api/example/annot_rec/MyTestRecord.java
+++ src/test/java/io/github/hglabplh_tech/reflect/clojure/api/example/annot_rec/MyTestRecord.java
@@ -1,6 +1,6 @@
 package io.github.hglabplh_tech.reflect.clojure.api.example.annot_rec;
 
-public record MyTestRecord(int test,
+public record MyTestRecord(Integer test,
                            String firstName,
                            String lastName) {
 }
